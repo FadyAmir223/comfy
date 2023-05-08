74b46d522f6db878bb7eefe217308b133610337c
FadyAmir223 <fadyamir223@gmail.com>
2023-05-08 22:10:41 +0300

    basic translation

--- client/i18n.js
+++ client/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 const resources = {
   en: {
@@ -14,12 +15,13 @@ const resources = {
 };
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     lng: 'en',
     fallbackLng: 'en',
-    resources,
+    // resources,
     interpolation: {
       escapeValue: false,
     },
